51410e65e644f891a831c768ba64346214192f56
Charlie Quick <cquickreal00@gmail.com>
2022-05-16 11:23:41 -0400

    Added computer exclusion ability.
    Added variables and modified if/elseif statements for optional use if the user has computers that need excluded. Also added more documentation through comments for this info.

--- PSLocalAdminAccountAdjustment.ps1
+++ PSLocalAdminAccountAdjustment.ps1
@@ -1,11 +1,19 @@
 ### Initialize Global Variables
 $computers = Get-ADComputer -Filter * | ForEach-Object {$_.Name} | Sort-Object
 $session = $null
+# $dc1 = "Your DC1 Name Here"
+# $dc2 = "Your DC2 Name Here"
+# $excludeComputer1 = "Your Excluded Computer Name Here"
 
 ### Loop through all Domain Computers and set the Password Never Expires flag to False if possible for the desired account, create the account if it doesn't exist, 
 ### make sure the account is in the Administrators group, and disable the built-in local Administrator account if it's enabled
 foreach ($computer in $computers) {
     Write-Host "`n$computer" -ForegroundColor Green
+    ### If you need to exclude your Domain Controllers or any other computers from this, do it in this if statement and the elseif statement below
+    ### Uncomment the first if statement and comment the normal one out, then uncomment the elseif statement below,
+    ### then uncomment the variables dc1, dc2, and excludeComputer1 at the top of this script
+    ### You can expand or reduce these as needed, just create more variables or reduce the curent ones, then add or remove from the if statement and the elseif statement
+    # if ((Test-Connection -TargetName $computer -Count 1 -Quiet) -and ($computer -ne $dc1) -and ($computer -ne $dc2) -and ($computer -ne $excludeComputer1)) {
     if (Test-Connection -TargetName $computer -Count 1 -Quiet) {
         try {
             $session = New-PSSession -ComputerName $computer
@@ -43,6 +51,9 @@ foreach ($computer in $computers) {
         }
         $session = $null
     }
+    # elseif (($computer -eq $dc1) -or ($computer -eq $dc2) -or ($computer -eq $excludeComputer1)) {
+    #     Write-Host "The computer was excluded from the script!"
+    # }
     else {
         Write-Host "The computer is currently offline!"
     }
--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 This project was something I wrote up in a short time for work.
 
 ## **Upcoming Improvements**
-Get user inputs for variables.
-
+- Get user inputs for variables
+- Add options for excluding specific computers :white_check_mark:
